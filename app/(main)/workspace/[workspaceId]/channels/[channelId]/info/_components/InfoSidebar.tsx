0bc0ad4b9f64848b6cdc09be401a18309272131a
Manoj Santra <santramanoj1997@gmail.com>
2024-12-18 08:21:53 +0530

    working on

--- app/(main)/workspace/[workspaceId]/channels/[channelId]/info/_components/InfoSidebar.tsx
+++ app/(main)/workspace/[workspaceId]/channels/[channelId]/info/_components/InfoSidebar.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
 import React, { FC } from "react";
 
-type InfoSidebarProps = {};
-const InfoSidebar: FC<InfoSidebarProps> = (): JSX.Element => {
-  return <div>InfoSidebar</div>;
+type InfoSidebarProps = {
+  params: {
+    workspaceId: string;
+    channelId?: string;
+  };
+};
+const InfoSidebar: FC<InfoSidebarProps> = ({ params }): JSX.Element => {
+  const baseURL = `/workspace/${params.workspaceId}/channels/${params.channelId}/info`;
+  return (
+    <aside className="info-sidebar">
+      <nav className="info-sidebar-nav">
+        <ul className="flex flex-col gap-6">
+          {/* {InfoNavItems.map((self,index)=>{
+        const active = 
+
+
+      })} */}
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default InfoSidebar;
--- environment.ts
+++ environment.ts
@@ -26,3 +26,91 @@ export const apiAuthPrefix = "/api/auth";
  * @type {string}
  * ***/
 export const DEFAULT_LOGIN_REDIRECT = "/";
+
+export const InfoNavItems = [
+    {
+      name: "Dashboard",
+      icon: "dashboard.svg",
+      url: "/",
+    },
+    {
+      name: "Documents",
+      icon: "documents.svg",
+      url: "/documents",
+    },
+    {
+      name: "Images",
+      icon: "images.svg",
+      url: "/images",
+    },
+    {
+      name: "Media",
+      icon: "video.svg",
+      url: "/media",
+    },
+    {
+      name: "Others",
+      icon: "others.svg",
+      url: "/others",
+    },
+  ];
+  
+  export const actionsDropdownItems = [
+    {
+      label: "Rename",
+      icon: "edit.svg",
+      value: "rename",
+    },
+    {
+      label: "Details",
+      icon: "info.svg",
+      value: "details",
+    },
+    {
+      label: "Share",
+      icon: "share.svg",
+      value: "share",
+    },
+    {
+      label: "Download",
+      icon: "download.svg",
+      value: "download",
+    },
+    {
+      label: "Delete",
+      icon: "delete.svg",
+      value: "delete",
+    },
+  ];
+  
+  export const sortTypes = [
+    {
+      label: "Date created (newest)",
+      value: "$createdAt-desc",
+    },
+    {
+      label: "Created Date (oldest)",
+      value: "$createdAt-asc",
+    },
+    {
+      label: "Name (A-Z)",
+      value: "name-asc",
+    },
+    {
+      label: "Name (Z-A)",
+      value: "name-desc",
+    },
+    {
+      label: "Size (Highest)",
+      value: "size-desc",
+    },
+    {
+      label: "Size (Lowest)",
+      value: "size-asc",
+    },
+  ];
+  
+  export const avatarPlaceholderUrl =
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg";
+  
+  export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
